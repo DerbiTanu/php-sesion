ca5b435ee61bbeef6f7fc0f66579aafdd5150250
DerbiTanu <derbitanu88@gmail.com>
2022-06-14 14:02:33 +0700

    upload

--- SEMESTER 3/Toko/home_info.php
+++
@@ -1,5 +0,0 @@
-<table width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
-  <tr>
-    <td align="center" valign="top"><img src="Gambar/Logo2.png" width="400" height="321" /></td>
-  </tr>
-  </table>
--- SEMESTER 3/Toko/logout.php
+++
@@ -1,5 +0,0 @@
-<?php session_start();
-if(isset($_SESSION['user']))
-	unset($_SESSION['user']);
-	echo "<meta http-equiv='refresh' content='0;index.php'>";
-?>
